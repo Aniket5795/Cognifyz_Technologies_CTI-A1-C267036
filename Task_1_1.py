dabc74e9532d213ee67707533ddaa2b125eb0063
Aniket5795 <sethianiket5795@gmail.com>
2026-02-06 19:15:10 +0530

    Task edited and uploaded

--- Task_1_1.py
+++ Task_1_1.py
@@ -1,13 +1,18 @@
+#TASK: Top Cuisines
 import pandas as pd
 
+#Determine the top three mostcommon cuisines in the dataset
+
 # -------LOAD DATASET--------
-df = pd.read_csv("Dataset .csv")
+df = pd.read_csv("Dataset.csv")
 
 # ------SPLIT CUISINES---------
 cuisine = df['Cuisines'].dropna().str.split(',').explode()
 
 #--------TOP 3 CUISINES----------
 top_cuisines = cuisine.value_counts().head(3)
+
+#Calculate the percentage ofrestaurants that serve each of the top cuisines
 
 #-------PERCENTAGE CALCULATION---------
 total_restaurants = len(df)
